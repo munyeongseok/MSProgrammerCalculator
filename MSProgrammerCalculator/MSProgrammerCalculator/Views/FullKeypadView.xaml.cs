1df5c6973721ec69a85de092fab93dc5fc2f6288
yeongseok mun <munyeongseok@gmail.com>
2022-08-22 21:48:19 +0900

    폴더 정리

--- MSProgrammerCalculator/MSProgrammerCalculator/Views/FullKeypadView.xaml.cs
+++ MSProgrammerCalculator/MSProgrammerCalculator/Views/FullKeypadView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MSProgrammerCalculator
+namespace MSProgrammerCalculator.Views
 {
     /// <summary>
     /// FullKeypadView.xaml에 대한 상호 작용 논리
