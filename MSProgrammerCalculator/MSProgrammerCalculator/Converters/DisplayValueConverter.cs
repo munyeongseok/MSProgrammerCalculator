1ca80ca873e41f65b32f2982347001f5482250a4
yeongseok mun <munyeongseok@gmail.com>
2022-10-03 19:21:00 +0900

    컨버터명 변경 및 Decimal 값에 콤마 표시

--- MSProgrammerCalculator/MSProgrammerCalculator/Converters/DisplayValueConverter.cs
+++ MSProgrammerCalculator/MSProgrammerCalculator/Converters/DisplayValueConverter.cs
@@ -10,11 +10,11 @@ namespace MSProgrammerCalculator.Converters
 {
     public class DisplayValueConverter : IMultiValueConverter
     {
-        private static readonly ValueToBaseNumberConverter _valueToBaseNumberConverter = new ValueToBaseNumberConverter();
+        private static readonly LongToBaseNumberConverter longToBaseNumberConverter = new LongToBaseNumberConverter();
 
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return _valueToBaseNumberConverter.Convert(values[0], targetType, values[1], culture); 
+            return longToBaseNumberConverter.Convert(values[0], targetType, values[1], culture); 
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
---
+++ MSProgrammerCalculator/MSProgrammerCalculator/Converters/LongToBaseNumberConverter.cs
@@ -0,0 +1,40 @@
+﻿using MSProgrammerCalculator.Common;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MSProgrammerCalculator.Converters
+{
+    public class LongToBaseNumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is long longValue && parameter is BaseNumber baseNumber)
+            {
+                switch (baseNumber)
+                {
+                    case BaseNumber.Binary:
+                        return Regex.Replace(System.Convert.ToString(longValue, 2), ".{4}", "$0 ");
+                    case BaseNumber.Octal:
+                        return Regex.Replace(System.Convert.ToString(longValue, 8), ".{3}", "$0 ");
+                    case BaseNumber.Decimal:
+                        return longValue.ToString("N0");
+                    case BaseNumber.Hexadecimal:
+                        return Regex.Replace(System.Convert.ToString(longValue, 16), ".{4}", "$0 ");
+                }
+            }
+            
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
