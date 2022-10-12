42ada432fdfa90829be8e6fdd75dd94116eb9e96
yeongseok mun <munyeongseok@gmail.com>
2022-10-12 23:00:35 +0900

    숫자 키패드의 1~9, A~F 버튼 더하기 기능 구현

--- MSProgrammerCalculator/MSProgrammerCalculator/ViewModels/CalculatorViewModel.cs
+++ MSProgrammerCalculator/MSProgrammerCalculator/ViewModels/CalculatorViewModel.cs
@@ -7,9 +7,34 @@ using System.Threading.Tasks;
 
 namespace MSProgrammerCalculator.ViewModels
 {
-    public class CalculatorViewModel
+    public class CalculatorViewModel : BindableBase
     {
+        private long displayValue;
+        public long DisplayValue
+        {
+            get => displayValue;
+            set => SetProperty(ref displayValue, value);
+        }
+
+        private BaseNumber selectedBaseNumber;
+        public BaseNumber SelectedBaseNumber
+        {
+            get => selectedBaseNumber;
+            set
+            {
+                if (SetProperty(ref selectedBaseNumber, value))
+                {
+                    SelectedBaseNumberChanged();
+                }
+            }
+        }
+
         public DelegateCommand NumericKeypadButtonClickCommand { get; private set; }
+
+        private const long MSB1000 = unchecked((long)0b_1000000000000000_0000000000000000_0000000000000000_0000000000000000);
+        private const long MSB1110 = unchecked((long)0b_1110000000000000_0000000000000000_0000000000000000_0000000000000000);
+        private const long MSB1111 = unchecked((long)0b_1111000000000000_0000000000000000_0000000000000000_0000000000000000);
+        private long _trackingValue;
 
         public CalculatorViewModel()
         {
@@ -18,26 +43,24 @@ namespace MSProgrammerCalculator.ViewModels
 
         private void NumericKeypadButtonClicked(object parameter)
         {
-            System.Diagnostics.Debug.WriteLine(parameter.ToString());
-
             switch ((NumericKeypadKeys)parameter)
             {
-                case NumericKeypadKeys.Num0: break;
-                case NumericKeypadKeys.Num1: break;
-                case NumericKeypadKeys.Num2: break;
-                case NumericKeypadKeys.Num3: break;
-                case NumericKeypadKeys.Num4: break;
-                case NumericKeypadKeys.Num5: break;
-                case NumericKeypadKeys.Num6: break;
-                case NumericKeypadKeys.Num7: break;
-                case NumericKeypadKeys.Num8: break;
-                case NumericKeypadKeys.Num9: break;
-                case NumericKeypadKeys.NumA: break;
-                case NumericKeypadKeys.NumB: break;
-                case NumericKeypadKeys.NumC: break;
-                case NumericKeypadKeys.NumD: break;
-                case NumericKeypadKeys.NumE: break;
-                case NumericKeypadKeys.NumF: break;
+                case NumericKeypadKeys.Num0: AddNumber(0); break;
+                case NumericKeypadKeys.Num1: AddNumber(1); break;
+                case NumericKeypadKeys.Num2: AddNumber(2); break;
+                case NumericKeypadKeys.Num3: AddNumber(3); break;
+                case NumericKeypadKeys.Num4: AddNumber(4); break;
+                case NumericKeypadKeys.Num5: AddNumber(5); break;
+                case NumericKeypadKeys.Num6: AddNumber(6); break;
+                case NumericKeypadKeys.Num7: AddNumber(7); break;
+                case NumericKeypadKeys.Num8: AddNumber(8); break;
+                case NumericKeypadKeys.Num9: AddNumber(9); break;
+                case NumericKeypadKeys.NumA: AddNumber(10); break;
+                case NumericKeypadKeys.NumB: AddNumber(11); break;
+                case NumericKeypadKeys.NumC: AddNumber(12); break;
+                case NumericKeypadKeys.NumD: AddNumber(13); break;
+                case NumericKeypadKeys.NumE: AddNumber(14); break;
+                case NumericKeypadKeys.NumF: AddNumber(15); break;
                 case NumericKeypadKeys.OpLeftShift: break;
                 case NumericKeypadKeys.OpRightShift: break;
                 case NumericKeypadKeys.OpModulo: break;
@@ -53,6 +76,48 @@ namespace MSProgrammerCalculator.ViewModels
                 case NumericKeypadKeys.OpDecimalSeparator: break;
                 case NumericKeypadKeys.OpResult: break;
             }
+        }
+
+        private void AddNumber(long number)
+        {
+            var value = _trackingValue;
+            switch (SelectedBaseNumber)
+            {
+                case BaseNumber.Binary:
+                    if ((value & MSB1000) == 0)
+                    {
+                        value = (value << 1) + number;
+                    }
+                    break;
+                case BaseNumber.Octal:
+                    if ((value & MSB1110) == 0)
+                    {
+                        value = (value << 3) + number;
+                    }
+                    break;
+                case BaseNumber.Decimal:
+                    try
+                    {
+                        value = checked(value * 10) + number;
+                    }
+                    catch (OverflowException)
+                    {
+                    }
+                    break;
+                case BaseNumber.Hexadecimal:
+                    if ((value & MSB1111) == 0)
+                    {
+                        value = (value << 4) + number;
+                    }
+                    break;
+            }
+
+            DisplayValue = _trackingValue = value;
+        }
+
+        private void SelectedBaseNumberChanged()
+        {
+            _trackingValue = 0;
         }
     }
 }
