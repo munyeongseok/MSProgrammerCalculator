1cd470bbf94246807429be4370fbb43dd22f44b5
yeongseok mun <munyeongseok@gmail.com>
2023-04-10 02:14:52 +0900

    단위 테스트 추가

--- MSProgrammerCalculator/CalculatorTests/UnitTest1.cs
+++ MSProgrammerCalculator/CalculatorTests/UnitTest1.cs
@@ -172,76 +172,8 @@ namespace CalculatorTests
             Assert.AreEqual("1 = ", calculator.Expression);
         }
 
-        [TestMethod("[5, \"4 + 1 = \"]")]
-        public void TestMethod8()
-        {
-            var calculator = new Calculator.Calculator();
-
-            calculator.EnqueueToken(Numbers.Num1);
-            calculator.EnqueueToken(Operators.Plus);
-            calculator.Evaluate();
-            Assert.AreEqual(1, calculator.Operand);
-            Assert.AreEqual("1 + ", calculator.Expression);
-
-            calculator.EnqueueToken(Operators.Submit);
-            calculator.Evaluate();
-            Assert.AreEqual(2, calculator.Operand);
-            Assert.AreEqual("1 + 1 = ", calculator.Expression);
-
-            calculator.EnqueueToken(Operators.Submit);
-            calculator.Evaluate();
-            Assert.AreEqual(3, calculator.Operand);
-            Assert.AreEqual("2 + 1 = ", calculator.Expression);
-
-            calculator.EnqueueToken(Operators.Submit);
-            calculator.Evaluate();
-            Assert.AreEqual(4, calculator.Operand);
-            Assert.AreEqual("3 + 1 = ", calculator.Expression);
-
-            calculator.EnqueueToken(Operators.Submit);
-            calculator.Evaluate();
-            Assert.AreEqual(5, calculator.Operand);
-            Assert.AreEqual("4 + 1 = ", calculator.Expression);
-        }
-
-        [TestMethod("[30, \"24 + 6 = \"]")]
-        public void TestMethod9()
-        {
-            var calculator = new Calculator.Calculator();
-
-            calculator.EnqueueToken(Numbers.Num1);
-            calculator.EnqueueToken(Operators.Plus);
-            calculator.EnqueueToken(Numbers.Num2);
-            calculator.EnqueueToken(Operators.Plus);
-            calculator.EnqueueToken(Numbers.Num3);
-            calculator.EnqueueToken(Operators.Plus);
-            calculator.Evaluate();
-            Assert.AreEqual(6, calculator.Operand);
-            Assert.AreEqual("1 + 2 + 3 + ", calculator.Expression);
-
-            calculator.EnqueueToken(Operators.Submit);
-            calculator.Evaluate();
-            Assert.AreEqual(12, calculator.Operand);
-            Assert.AreEqual("1 + 2 + 3 + 6 = ", calculator.Expression);
-
-            calculator.EnqueueToken(Operators.Submit);
-            calculator.Evaluate();
-            Assert.AreEqual(18, calculator.Operand);
-            Assert.AreEqual("12 + 6 = ", calculator.Expression);
-
-            calculator.EnqueueToken(Operators.Submit);
-            calculator.Evaluate();
-            Assert.AreEqual(24, calculator.Operand);
-            Assert.AreEqual("18 + 6 = ", calculator.Expression);
-
-            calculator.EnqueueToken(Operators.Submit);
-            calculator.Evaluate();
-            Assert.AreEqual(30, calculator.Operand);
-            Assert.AreEqual("24 + 6 = ", calculator.Expression);
-        }
-
         [TestMethod("[6, \"( 1 + ( 2 + 3 ) ) = \"]")]
-        public void TestMethod10()
+        public void TestMethod8()
         {
             var calculator = new Calculator.Calculator();
 
@@ -265,7 +197,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[10, \"( 1 + 2 ) + ( 3 + 4 ) + ( 0 ) = \"]")]
-        public void TestMethod11()
+        public void TestMethod9()
         {
             var calculator = new Calculator.Calculator();
 
@@ -293,7 +225,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[-27, \"9 - 6 × 6 = \"]")]
-        public void TestMethod12()
+        public void TestMethod10()
         {
             var calculator = new Calculator.Calculator();
 
@@ -321,7 +253,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[2, \"1 + ( 2 ) \"] -> Clear -> [0, \"1 + \"]")]
-        public void TestMethod13()
+        public void TestMethod11()
         {
             var calculator = new Calculator.Calculator();
 
@@ -344,7 +276,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[-2, \"NOT( NOT( NOT( NOT( NOT( 1 ) ) ) ) ) \"")]
-        public void TestMethod14()
+        public void TestMethod12()
         {
             var calculator = new Calculator.Calculator();
 
@@ -376,7 +308,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[1, \"1 + \"] -> [1, \"1 - \"] -> [1, \"1 × \"] -> [1, \"1 ÷ \"] -> [1, \"1 % \"]")]
-        public void TestMethod15()
+        public void TestMethod13()
         {
             var calculator = new Calculator.Calculator();
 
@@ -408,7 +340,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[0, \"0 + \"] -> Clear -> [0, \"0 - \"] -> Clear -> [0, \"0 × \"] -> Clear -> [0, \"0 ÷ \"] -> Clear -> [0, \"0 % \"]")]
-        public void TestMethod16()
+        public void TestMethod14()
         {
             var calculator = new Calculator.Calculator();
 
@@ -443,7 +375,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[2, \"1 + 2 - 3 × 4 ÷ 5 % \"]")]
-        public void TestMethod17()
+        public void TestMethod15()
         {
             var calculator = new Calculator.Calculator();
 
@@ -484,7 +416,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[0, \"1 × ( \"]")]
-        public void TestMethod18()
+        public void TestMethod16()
         {
             var calculator = new Calculator.Calculator();
 
@@ -501,7 +433,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[-2, \"NOT( 1 ) × ( \"]")]
-        public void TestMethod19()
+        public void TestMethod17()
         {
             var calculator = new Calculator.Calculator();
 
@@ -518,7 +450,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[-2, \"1 + NOT( 1 ) × ( \"]")]
-        public void TestMethod20()
+        public void TestMethod18()
         {
             var calculator = new Calculator.Calculator();
 
@@ -536,7 +468,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[1, \"( 1 ) × ( \"]")]
-        public void TestMethod21()
+        public void TestMethod19()
         {
             var calculator = new Calculator.Calculator();
 
@@ -554,7 +486,7 @@ namespace CalculatorTests
         }
 
         [TestMethod("[-1, \"1 + negate( 1 ) \"] -> [-1, \"1 + ( \"]")]
-        public void TestMethod22()
+        public void TestMethod20()
         {
             var calculator = new Calculator.Calculator();
 
@@ -569,6 +501,177 @@ namespace CalculatorTests
             calculator.Evaluate();
             Assert.AreEqual(-1, calculator.Operand);
             Assert.AreEqual("1 + ( ", calculator.Expression);
+        }
+    }
+
+    /// <summary>
+    /// [Display Operand, Expression]
+    /// </summary>
+    [TestClass]
+    public class UnitTest_RepeatedSubmit
+    {
+        [TestMethod("[30, \"24 + 6 = \"]")]
+        public void TestMethod1()
+        {
+            var calculator = new Calculator.Calculator();
+
+            calculator.EnqueueToken(Numbers.Num1);
+            calculator.EnqueueToken(Operators.Plus);
+            calculator.EnqueueToken(Numbers.Num2);
+            calculator.EnqueueToken(Operators.Plus);
+            calculator.EnqueueToken(Numbers.Num3);
+            calculator.EnqueueToken(Operators.Plus);
+            calculator.Evaluate();
+            Assert.AreEqual(6, calculator.Operand);
+            Assert.AreEqual("1 + 2 + 3 + ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(12, calculator.Operand);
+            Assert.AreEqual("1 + 2 + 3 + 6 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(18, calculator.Operand);
+            Assert.AreEqual("12 + 6 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(24, calculator.Operand);
+            Assert.AreEqual("18 + 6 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(30, calculator.Operand);
+            Assert.AreEqual("24 + 6 = ", calculator.Expression);
+        }
+
+        [TestMethod("[5, \"4 + 1 = \"]")]
+        public void TestMethod2()
+        {
+            var calculator = new Calculator.Calculator();
+
+            calculator.EnqueueToken(Numbers.Num1);
+            calculator.EnqueueToken(Operators.Plus);
+            calculator.Evaluate();
+            Assert.AreEqual(1, calculator.Operand);
+            Assert.AreEqual("1 + ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(2, calculator.Operand);
+            Assert.AreEqual("1 + 1 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(3, calculator.Operand);
+            Assert.AreEqual("2 + 1 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(4, calculator.Operand);
+            Assert.AreEqual("3 + 1 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(5, calculator.Operand);
+            Assert.AreEqual("4 + 1 = ", calculator.Expression);
+        }
+
+        [TestMethod("[5, \"-3 - 2 = \"]")]
+        public void TestMethod3()
+        {
+            var calculator = new Calculator.Calculator();
+
+            calculator.EnqueueToken(Numbers.Num1);
+            calculator.EnqueueToken(Operators.Minus);
+            calculator.EnqueueToken(Numbers.Num2);
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(-1, calculator.Operand);
+            Assert.AreEqual("1 - 2 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(-3, calculator.Operand);
+            Assert.AreEqual("-1 - 2 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(-5, calculator.Operand);
+            Assert.AreEqual("-3 - 2 = ", calculator.Expression);
+        }
+
+        [TestMethod("[8, \"4 × 2 = \"]")]
+        public void TestMethod4()
+        {
+            var calculator = new Calculator.Calculator();
+
+            calculator.EnqueueToken(Numbers.Num1);
+            calculator.EnqueueToken(Operators.Multiply);
+            calculator.EnqueueToken(Numbers.Num2);
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(2, calculator.Operand);
+            Assert.AreEqual("1 × 2 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(4, calculator.Operand);
+            Assert.AreEqual("2 × 2 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(8, calculator.Operand);
+            Assert.AreEqual("4 × 2 = ", calculator.Expression);
+        }
+
+        [TestMethod("[0, \"0 ÷ 2 = \"]")]
+        public void TestMethod5()
+        {
+            var calculator = new Calculator.Calculator();
+
+            calculator.EnqueueToken(Numbers.Num1);
+            calculator.EnqueueToken(Operators.Divide);
+            calculator.EnqueueToken(Numbers.Num2);
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(0, calculator.Operand);
+            Assert.AreEqual("1 ÷ 2 = ", calculator.Expression);
+            
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(0, calculator.Operand);
+            Assert.AreEqual("0 ÷ 2 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(0, calculator.Operand);
+            Assert.AreEqual("0 ÷ 2 = ", calculator.Expression);
+        }
+
+        [TestMethod("[1, \"1 % 2 = \"]")]
+        public void TestMethod6()
+        {
+            var calculator = new Calculator.Calculator();
+
+            calculator.EnqueueToken(Numbers.Num1);
+            calculator.EnqueueToken(Operators.Modulo);
+            calculator.EnqueueToken(Numbers.Num2);
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(1, calculator.Operand);
+            Assert.AreEqual("1 % 2 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(1, calculator.Operand);
+            Assert.AreEqual("1 % 2 = ", calculator.Expression);
+
+            calculator.EnqueueToken(Operators.Submit);
+            calculator.Evaluate();
+            Assert.AreEqual(1, calculator.Operand);
+            Assert.AreEqual("1 % 2 = ", calculator.Expression);
         }
     }
 }
